c476e508e03cc038b9987ce89299d547972b6be0
Natavan Hasanova <natavanhesenova4@gmail.com>
2024-02-05 02:14:30 +0400

    Create a utility class for establishing PostgreSQL database connection using JDBC.

--- src/Connectivity/Database_Connection.java
+++ src/Connectivity/Database_Connection.java
@@ -1,2 +1,21 @@
-package src.Connectivity;public class Database_Connection {
+package src.Connectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class Database_Connection {
+    public static Connection connect() {
+        String url = "jdbc:postgresql://localhost:5432/DatabaseName";
+        String user = "username";
+        String password = "password";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to Database successfully.");
+        } catch (SQLException var5) {
+            System.out.println(var5.getMessage());
+        }
+        return con;
+    }
 }
