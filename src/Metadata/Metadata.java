5c97611e8b520e3edd107b03a90168369e38d022
Natavan Hasanova <natavanhesenova4@gmail.com>
2024-02-05 02:25:24 +0400

    Add Metadata class for retrieving and printing database metadata.

---
+++ src/Metadata/Metadata.java
@@ -0,0 +1,88 @@
+package src.Metadata;
+
+import src.Connectivity.Database_Connection;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Metadata extends Database_Connection {
+    public static void printTableNamesandColumns() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            System.out.println(tableName);
+            System.out.println();
+            printColumns(tableName);
+            System.out.println();
+        }
+    }
+
+    public static void printColumns(String tableName) throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet columns = metaData.getColumns(null, null, tableName, null);
+        while (columns.next()) {
+            String columnName = columns.getString("COLUMN_NAME");
+            System.out.println(columnName);
+        }
+    }
+
+    public static void printColumnDetails() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            ResultSet columns = metaData.getColumns(null, null, tableName, null);
+            System.out.println(tableName);
+            System.out.println();
+            while (columns.next()) {
+                String columnName = columns.getString("COLUMN_NAME");
+                String columnType = columns.getString("TYPE_NAME");
+                System.out.println(columnName + " " + columnType);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printPrimaryKeys() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            ResultSet primaryKeys = metaData.getPrimaryKeys(null, null, tableName);
+            System.out.println(tableName);
+            System.out.println();
+            while (primaryKeys.next()) {
+                String primaryKeyColumnName = primaryKeys.getString("COLUMN_NAME");
+                System.out.println(primaryKeyColumnName);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printForeignKeys() throws SQLException {
+        Connection connection = connect();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (tables.next()) {
+            String tableName = tables.getString("TABLE_NAME");
+            ResultSet foreignKeys = metaData.getImportedKeys(null, null, tableName);
+            System.out.println(tableName);
+            System.out.println();
+            while (foreignKeys.next()) {
+                String foreign_key_column = foreignKeys.getString("FKCOLUMN_NAME");
+                String referenced_table_name = foreignKeys.getString("PKTABLE_NAME");
+                String referenced_column_name = foreignKeys.getString("PKCOLUMN_NAME");
+                System.out.print(foreign_key_column + " references " + referenced_table_name + "(" + referenced_column_name + ")");
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
