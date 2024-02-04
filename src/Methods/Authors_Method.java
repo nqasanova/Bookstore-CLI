84ac9b31bce730f7261ce1573f6086966053a0db
Natavan Hasanova <natavanhesenova4@gmail.com>
2024-02-05 02:28:59 +0400

    Add CRUD functionality for managing authors.

---
+++ src/Methods/Authors_Method.java
@@ -0,0 +1,94 @@
+package src.Methods;
+import src.Connectivity.Database_Connection;
+import src.Entity.Authors;
+
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Authors_Method extends Database_Connection {
+    public static boolean addAuthors(Authors authors) {
+        try (Connection connection = connect();) {
+            PreparedStatement st = connection.prepareStatement("INSERT INTO authors (author_id,author_name) VALUES (?,?)");
+            st.setInt(1, authors.getAuthor_id());
+            st.setString(2, authors.getAuthor_name());
+            System.out.println("Inserting author");
+            st.execute();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            return false;
+        }
+        System.out.println("Author inserted successfully");
+        return true;
+    }
+
+    public static List<Authors> getAllAuthors() {
+        List<Authors> authors = new ArrayList<>();
+        try (Connection connection = connect()) {
+            Statement st = connection.createStatement();
+            st.execute("SELECT * FROM authors");
+            ResultSet res = st.getResultSet();
+            while (res.next()) {
+                int author_id = res.getInt("author_id");
+                String author_name = res.getString("author_name");
+                System.out.println("author_id = " + author_id + ", author_name = " + author_name);
+                authors.add(new Authors(author_id, author_name));
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+        return authors;
+    }
+
+    public static boolean updateAuthors(Authors authors) {
+        try (Connection connection = connect()) {
+            PreparedStatement st = connection.prepareStatement("UPDATE authors SET author_name=? WHERE author_id=?");
+            st.setString(1, authors.getAuthor_name());
+            st.setInt(2, authors.getAuthor_id());
+            System.out.println("Updated successfully");
+            st.execute();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteAuthors(int author_id) {
+        try (Connection connection = connect()) {
+            Statement st = connection.createStatement();
+            st.execute("DELETE FROM authors WHERE author_id = " + author_id);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            return false;
+        }
+        System.out.println("Successfully Deleted author");
+        return true;
+    }
+
+    public static Authors getAuthorById(int author_id) {
+        Authors authors = null;
+        try (Connection connection = connect()) {
+            Statement st = connection.createStatement();
+            st.execute("SELECT * FROM authors WHERE author_id = " + author_id);
+            ResultSet res = st.getResultSet();
+            while (res.next()) {
+                int id = res.getInt("author_id");
+                String author_name = res.getString("author_name");
+                System.out.println("author_id = " + author_id + ", author_name = " + author_name);
+                authors = new Authors(id, author_name);
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+        return authors;
+    }
+}
